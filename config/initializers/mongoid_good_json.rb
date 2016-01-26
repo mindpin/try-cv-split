906a85a4bed7404c95c1adc042ffc1c898ecbe59
destinyd <destinyd_war@163.com>
2016-01-26 11:24:26 +0000

    补充Mongoid::Criteria以及Array时的as_good_json、to_good_json

--- config/initializers/mongoid_good_json.rb
+++ config/initializers/mongoid_good_json.rb
@@ -1,14 +1,34 @@
 module Mongoid
   module Document
-    def good_as_json(options=nil)
+    def as_good_json(options=nil)
       tmp_json = as_json(options)
       tmp_json['id'] = tmp_json['_id'].to_s if tmp_json['_id']
       tmp_json.delete '_id'
       tmp_json
     end
 
-    def good_to_json(options=nil)
-      good_as_json(options).to_json
+    def to_good_json(options=nil)
+      as_good_json(options).to_json
     end
+  end
+
+  class Criteria
+    def as_good_json(options = nil)
+      entries.as_good_json(options)
+    end
+
+    def to_good_json(options = nil)
+      entries.to_good_json(options)
+    end
+  end
+end
+
+class Array
+  def as_good_json(options = nil) #:nodoc:
+    map { |v| options ? v.as_good_json(options.dup) : v.as_good_json }
+  end
+
+  def to_good_json(options = nil) #:nodoc:
+    map { |v| options ? v.to_good_json(options.dup) : v.to_good_json }
   end
 end
